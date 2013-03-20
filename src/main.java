663342363a24ec15d27e7163f3ec75c2640c8365
amira <amira@el_nada>
2013-03-20 11:16:05 +0200

    first commit

---
+++ src/main.java
@@ -0,0 +1,15 @@
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public int add(int x, int y) {
+		return x + y;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
